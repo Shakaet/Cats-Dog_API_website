058a59c0be96fc1a43dbcea6be3e2fb343d2f005
Shakaet <abdshakaet@gmail.com>
2024-10-05 23:23:57 +0600

    add

--- index.js
+++ index.js
@@ -10,6 +10,35 @@
  navData()
 
 
+ let loadAllData=async()=>{
+     document.getElementById("spinner").style.display="none"
+     document.getElementById("card").style.display="grid"
+
+     let res= await fetch("https://openapi.programming-hero.com/api/peddy/pets")
+     let data= await res.json()
+    console.log(data)
+
+
+
+
+    
+
+   
+
+
+ }
+
+
+ let handleSpinner=()=>{
+    document.getElementById("spinner").style.display="block"
+    document.getElementById("card").style.display="none"
+    
+    setTimeout(function(){
+        loadAllData()
+    },2000)
+ }
+
+
  let navDisplayData=(items)=>{
 
     let nav_container =document.getElementById("categories")
@@ -27,7 +56,7 @@
 
         
 
-        <button class="btn m-3  btn-category text-xl font-bold">
+        <button onclick="handleSpinner()" class="btn m-3  btn-category text-xl font-bold">
 
          <img class="w-7 h-7" src="${item.category_icon}">
         
