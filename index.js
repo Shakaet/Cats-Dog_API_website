c7f1867123100a027337f2eb864020e9b6afd99e
Shakaet <abdshakaet@gmail.com>
2024-10-06 22:33:46 +0600

    add

--- index.js
+++ index.js
@@ -16,14 +16,57 @@
 
      let res= await fetch("https://openapi.programming-hero.com/api/peddy/pets")
      let data= await res.json()
-    console.log(data)
+    displayAllData(data.pets)
 
 
 
+ }
 
-    
+ loadAllData()
 
-   
+ let displayAllData= (items)=> {
+
+
+   let item_container=document.getElementById("card")
+
+      for(let item of items){
+
+         let breed = item.breed ? item.breed : "Breed not available";
+         let birthDate = item.date_of_birth ? item.date_of_birth : "Date of birth not available";
+         let gender = item.gender ? item.gender : "Gender not specified";
+         let price = item.price ? `$${item.price}` : "Price not available";
+
+
+         let cardDiv=document.createElement("div")
+
+         cardDiv.innerHTML=`
+
+        
+
+         <img src="${item.image}" alt="Mister Tartosh" class="w-full h-40 object-cover rounded-t-lg">
+            <h3 class="text-xl font-semibold mt-4">${item.pet_name}</h3>
+            <p class="text-sm text-gray-600">Breed: ${breed}</p>
+            <p class="text-sm text-gray-600">Birth: ${birthDate}</p>
+            <p class="text-sm text-gray-600">Gender: ${gender}</p>
+            <p class="text-sm font-semibold text-green-500">Price: ${price}</p>
+            <div class="mt-4 flex justify-between">
+               <button class="bg-blue-500 text-white py-1 px-3 rounded-md">Adopt</button>
+               <button class="bg-blue-500 text-white py-1 px-3 rounded-md">Adopt</button>
+               <button class="bg-gray-200 text-gray-700 py-1 px-3 rounded-md">Details</button>
+            </div>
+
+         
+         
+         
+         `
+         item_container.appendChild(cardDiv)
+
+
+
+         
+
+         
+      }
 
 
  }
@@ -42,13 +85,13 @@
  let navDisplayData=(items)=>{
 
     let nav_container =document.getElementById("categories")
-    nav_container.classList=" flex justify-evenly py-2 shadow-xl"
+    nav_container.classList=" md:flex justify-evenly mx-auto shadow-xl"
 
     for(let item of items){
         console.log(item.category)
 
         let btn_div=document.createElement("div")
-        // btn_div.classList="flex"
+        // btn_div.classList="py-10"
 
         btn_div.innerHTML=`
 
@@ -56,7 +99,7 @@
 
         
 
-        <button onclick="handleSpinner()" class="btn m-3  btn-category text-xl font-bold">
+        <button onclick="handleSpinner()" class="btn m-3 btn-category text-xl font-bold">
 
          <img class="w-7 h-7" src="${item.category_icon}">
         
